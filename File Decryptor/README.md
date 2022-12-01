bf84a6f58d36638e5c463b39937d626a7c81381d
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-12-01 11:12:32 +0600

    Add files via upload

---
+++ File Decryptor/README.md
@@ -0,0 +1,14 @@
+# Vault-Cracker
+Program for cracking NQ Vault
+https://play.google.com/store/apps/details?id=com.netqin.ps
+
+//
+Disclaimer:
+
+This program is for fun and educational purpose only.
+
+I never encourage to violate privacy.
+
+I will not responsible for any misuse of this code or program.
+
+//
---
+++ File Decryptor/about.c
@@ -0,0 +1,8 @@
+void typEff(char str[]);
+void about()
+{
+    typEff("\n\nHello!\nWelcome To File Decryptor.\n\n");
+    typEff("Author: Mahmudur Rahman Labib\nWeb: https://www.mahmudurrahmanlabib.com\n\n");
+    typEff("Disclaimer:\nThis program is made for educational purpose only.\n\n");
+    typEff("No User Limitations!\nThis is an open-source and free program.\n\n");
+}
